3e94895ded1e3c418d437078f0f30437c469c6b2
Александр <buga-91@inbox.ru>
2020-10-26 23:32:29 +0600

    initial commit

---
+++ vk-market/vk-market/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  vk-market
+//
+//  Created by Александр on 26.10.2020.
+//  Copyright © 2020 AlexBugatti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
